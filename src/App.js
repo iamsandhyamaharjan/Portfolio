7f29b2b2508d3f28f6160e8b28749cff68f1028c
iamsandhyamaharjan <98277290+iamsandhyamaharjan@users.noreply.github.com>
2022-10-10 11:16:13 +0545

    oops

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Route, Routes } from "react-router-dom"
+import './index.css';
+import Home from "./routes/Home"
+import Contact from "./routes/Contact"
+import Project from "./routes/Project"
+import About from "./routes/About"
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/project" element={<Project/>}/>
+      <Route path="/contact" element={<Contact />}/>
+    </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/WorkCardData.js
@@ -0,0 +1,26 @@
+import pro1 from "../assets/project1.png"
+import pro2 from "../assets/project2.png"
+import pro3 from "../assets/project3.png"
+
+const ProjectCardData = [
+    {
+        imgsrc:pro1,
+        title:"Ecommerce Website Design",
+        text:"Thriving Facebook community and top-ranked podcast, Amy Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates ipsam dolores nobis voluptatem! Dolore expedita assumenda temporibus perspiciatis explicabo? Totam dolores quo odio commodi aliquid consectetur sequi minima hic alias.",
+        view:"https://www.youtube.com"
+    },
+    {
+        imgsrc:pro2,
+        title:"Rymo Ecommerce Design",
+        text:"Thriving Facebook community and top-ranked podcast, Amy Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates ipsam dolores nobis voluptatem! Dolore expedita assumenda temporibus perspiciatis explicabo? Totam dolores quo odio commodi aliquid consectetur sequi minima hic alias.",
+        view:"https://www.youtube.com"
+    },
+    {
+        imgsrc:pro3,
+        title:"LMS/Course Website Design",
+        text:"Thriving Facebook community and top-ranked podcast, Amy Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates ipsam dolores nobis voluptatem! Dolore expedita assumenda temporibus perspiciatis explicabo? Totam dolores quo odio commodi aliquid consectetur sequi minima hic alias.",
+        view:"https://www.youtube.com"
+    }
+
+];
+export default ProjectCardData;
